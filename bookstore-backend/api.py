45a5286b71ca3783d073538b94985ee7fcef3af5
MWyssy <mike@wyss.co.uk>
2023-06-13 13:43:12 +0300

    add prometheus metrics

--- bookstore-backend/api.py
+++ bookstore-backend/api.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 from flask_cors import CORS
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
+
 
 # Initialize Flask
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+metrics = RESTfulPrometheusMetrics(app, api)
 
 # A List of Dicts to store all of the books
 books = [{
